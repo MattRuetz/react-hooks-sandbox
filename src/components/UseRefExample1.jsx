40dc802a8fb5a022f95d73030ace57637fdf7ce2
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-08 13:26:44 -0500

    initial

---
+++ src/components/UseRefExample1.jsx
@@ -0,0 +1,24 @@
+import { useRef } from 'react';
+
+function UseRefExample1() {
+    const inputRef = useRef();
+
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    ref={inputRef}
+                    id="name"
+                    className="form-control-mb-2"
+                />
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default UseRefExample1;
