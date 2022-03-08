d0df2fafcc3a1d9586892d3f77b1966b68dad86f
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-08 14:53:04 -0500

    fixed useEffect dep. array.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,24 +9,20 @@ function Todo() {
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos/1')
             .then((res) => res.json())
-            .then(
-                (data) => {
-                    setTimeout(() => {
-                        if (isMounted.current) {
-                            setTodo(data);
-                            setLoading(false);
-                        }
-                    }, 3000);
-                },
-                [isMounted]
-            );
+            .then((data) => {
+                setTimeout(() => {
+                    if (isMounted.current) {
+                        setTodo(data);
+                        setLoading(false);
+                    }
+                }, 3000);
+            });
 
-        // Runs when component is unmounted
+        // Runs when component is unmounted (<Todo /> removed from DOM)
         return () => {
-            //
             isMounted.current = false;
         };
-    }, []);
+    }, [isMounted]);
 
     return loading ? <h3>Loading...</h3> : <h1>{todo.title}</h1>;
 }
